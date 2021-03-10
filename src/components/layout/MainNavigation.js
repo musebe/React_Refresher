44e758422db084249f8a54b92bce0f8c92894edc
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro.local>
2021-03-10 14:10:35 +0300

    added context api

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
-import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-const MainNavigation = () => {
+import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorites-context';
+
+function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Meetups</div>
@@ -11,15 +16,20 @@ const MainNavigation = () => {
             <Link to='/'>Meetups</Link>
           </li>
           <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
+            <Link to='/new-meetup'>New Meetup</Link>
           </li>
           <li>
-            <Link to='/favorites'>Favorites</Link>
+            <Link to='/favorites'>
+              Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
     </header>
   );
-};
+}
 
 export default MainNavigation;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,8 @@
+import { useHistory } from 'react-router';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const history = useHistory();
   const addMeetupHandler = (meetupData) => {
     fetch('https://react-next-c11ab-default-rtdb.firebaseio.com/meetups.json', {
       method: 'POST',
@@ -8,6 +10,8 @@ const NewMeetupPage = () => {
       headers: {
         'Content-Type': 'application/json',
       },
+    }).then(() => {
+      history.replace('/');
     });
   };
   return (
